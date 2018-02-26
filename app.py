a109b5f07d334b23da9262620e0efb69b7884f40
slau8 <slau8@stuy.edu>
2018-02-26 12:43:49 -0500

    grgf

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, flash, url_for
+from utils import nasa
+
+app = Flask(__name__)
+app.secret_key = os.urandom(128)
+
+# Landing page; displays the home page
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
